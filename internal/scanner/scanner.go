3ba8bef64ee738b8ee556ca691b8e916cc0a4d17
Igor Klepacki <neg4n@icloud.com>
2025-06-21 16:44:08 +0200

    feat: parallel directory walking

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sync"
+	"sync/atomic"
 )
 
 type CleanupTarget struct {
@@ -20,6 +23,9 @@ type Scanner struct {
 	progressCallback func(float64)
 	totalDirs        int
 	processedDirs    int
+	numWorkers       int
+	targetsMutex     sync.RWMutex
+	progressMutex    sync.RWMutex
 }
 
 var CommonCleanupDirs = map[string]string{
@@ -50,24 +56,36 @@ func New() (*Scanner, error) {
 		return nil, fmt.Errorf("failed to get working directory: %w", err)
 	}
 
+	numWorkers := runtime.NumCPU()
+	if numWorkers > 8 {
+		numWorkers = 8
+	}
+	if numWorkers < 2 {
+		numWorkers = 2
+	}
+
 	return &Scanner{
 		workingDir: wd,
 		targets:    make([]CleanupTarget, 0),
+		numWorkers: numWorkers,
 	}, nil
 }
 
 func (s *Scanner) ScanWithProgress(progressCallback func(float64)) error {
+	s.targetsMutex.Lock()
 	s.targets = make([]CleanupTarget, 0)
+	s.targetsMutex.Unlock()
+	
+	s.progressMutex.Lock()
 	s.progressCallback = progressCallback
 	s.processedDirs = 0
-
-	s.totalDirs = s.countDirectories(s.workingDir)
+	s.progressMutex.Unlock()
 
 	if s.progressCallback != nil {
 		s.progressCallback(0.0)
 	}
 
-	err := s.scanRecursiveWithProgress(s.workingDir)
+	err := s.parallelScanWithProgress(s.workingDir)
 
 	if s.progressCallback != nil {
 		s.progressCallback(1.0)
@@ -76,99 +94,231 @@ func (s *Scanner) ScanWithProgress(progressCallback func(float64)) error {
 	return err
 }
 
-func (s *Scanner) countDirectories(dir string) int {
-	count := 0
+type workItem struct {
+	path string
+	info os.FileInfo
+}
+
+type scanResult struct {
+	target CleanupTarget
+	err    error
+}
+
+func (s *Scanner) parallelScanWithProgress(rootDir string) error {
+	workQueue := make(chan workItem, 1000)
+	resultQueue := make(chan scanResult, 100)
+	var wg sync.WaitGroup
+	var totalDirs int64
+	var processedDirs int64
+
+	wg.Add(s.numWorkers)
+	for i := 0; i < s.numWorkers; i++ {
+		go s.worker(workQueue, resultQueue, &wg, &processedDirs, &totalDirs)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultQueue)
+	}()
+
+	go func() {
+		defer close(workQueue)
+		s.walkDirectory(rootDir, workQueue, &totalDirs)
+	}()
+
+	for result := range resultQueue {
+		if result.err != nil {
+			continue
+		}
+		
+		if result.target.Path != "" {
+			s.targetsMutex.Lock()
+			s.targets = append(s.targets, result.target)
+			s.targetsMutex.Unlock()
+		}
+
+		if s.progressCallback != nil {
+			processed := atomic.LoadInt64(&processedDirs)
+			total := atomic.LoadInt64(&totalDirs)
+			if total > 0 {
+				progress := float64(processed) / float64(total)
+				if progress > 1.0 {
+					progress = 1.0
+				}
+				s.progressCallback(progress)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (s *Scanner) walkDirectory(dir string, workQueue chan<- workItem, totalDirs *int64) {
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return nil
 		}
+		
 		if info.IsDir() {
-			count++
+			atomic.AddInt64(totalDirs, 1)
+			select {
+			case workQueue <- workItem{path: path, info: info}:
+			default:
+			}
+			
+			if s.isCleanupTarget(info.Name()) {
+				return filepath.SkipDir
+			}
 		}
+		
 		return nil
 	})
-	return count
 }
 
-func (s *Scanner) scanRecursiveWithProgress(dir string) error {
-	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+func (s *Scanner) worker(workQueue <-chan workItem, resultQueue chan<- scanResult, wg *sync.WaitGroup, processedDirs *int64, totalDirs *int64) {
+	defer wg.Done()
+	
+	for item := range workQueue {
+		atomic.AddInt64(processedDirs, 1)
+		
+		if s.isCleanupTarget(item.info.Name()) {
+			size := s.calculateDirSizeConcurrent(item.path)
+			
+			target := CleanupTarget{
+				Path:     item.path,
+				Name:     item.info.Name(),
+				Size:     size,
+				Type:     s.getTargetType(item.info.Name()),
+				Selected: false,
+			}
+			
+			resultQueue <- scanResult{target: target, err: nil}
+		} else {
+			resultQueue <- scanResult{target: CleanupTarget{}, err: nil}
+		}
+	}
+}
+
+func (s *Scanner) calculateDirSizeConcurrent(dirPath string) int64 {
+	var size int64
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+	
+	semaphore := make(chan struct{}, s.numWorkers)
+	
+	filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return nil
 		}
-
+		
 		if !info.IsDir() {
-			return nil
+			wg.Add(1)
+			go func(fileSize int64) {
+				defer wg.Done()
+				semaphore <- struct{}{}
+				defer func() { <-semaphore }()
+				
+				mu.Lock()
+				size += fileSize
+				mu.Unlock()
+			}(info.Size())
 		}
-
-		s.processedDirs++
-		if s.progressCallback != nil && s.totalDirs > 0 {
-			progress := float64(s.processedDirs) / float64(s.totalDirs)
-			if progress > 1.0 {
-				progress = 1.0
-			}
-			s.progressCallback(progress)
-		}
-
-		if s.isCleanupTarget(info.Name()) {
-			size, err := s.calculateDirSize(path)
-			if err != nil {
-				size = 0
-			}
-
-			target := CleanupTarget{
-				Path:     path,
-				Name:     info.Name(),
-				Size:     size,
-				Type:     s.getTargetType(info.Name()),
-				Selected: false,
-			}
-
-			s.targets = append(s.targets, target)
-
-			return filepath.SkipDir
-		}
-
+		
 		return nil
 	})
+	
+	wg.Wait()
+	return size
 }
+
+
 
 func (s *Scanner) Scan() error {
+	s.targetsMutex.Lock()
 	s.targets = make([]CleanupTarget, 0)
-	return s.scanRecursive(s.workingDir)
+	s.targetsMutex.Unlock()
+	
+	return s.parallelScan(s.workingDir)
 }
 
-func (s *Scanner) scanRecursive(dir string) error {
-	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+func (s *Scanner) parallelScan(rootDir string) error {
+	workQueue := make(chan workItem, 1000)
+	resultQueue := make(chan scanResult, 100)
+	var wg sync.WaitGroup
+
+	wg.Add(s.numWorkers)
+	for i := 0; i < s.numWorkers; i++ {
+		go s.workerNoProgress(workQueue, resultQueue, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultQueue)
+	}()
+
+	go func() {
+		defer close(workQueue)
+		s.walkDirectoryNoProgress(rootDir, workQueue)
+	}()
+
+	for result := range resultQueue {
+		if result.err != nil {
+			continue
+		}
+		
+		if result.target.Path != "" {
+			s.targetsMutex.Lock()
+			s.targets = append(s.targets, result.target)
+			s.targetsMutex.Unlock()
+		}
+	}
+
+	return nil
+}
+
+func (s *Scanner) walkDirectoryNoProgress(dir string, workQueue chan<- workItem) {
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return nil
 		}
-
-		if !info.IsDir() {
-			return nil
-		}
-
-		if s.isCleanupTarget(info.Name()) {
-			size, err := s.calculateDirSize(path)
-			if err != nil {
-				size = 0
+		
+		if info.IsDir() {
+			select {
+			case workQueue <- workItem{path: path, info: info}:
+			default:
 			}
-
-			target := CleanupTarget{
-				Path:     path,
-				Name:     info.Name(),
-				Size:     size,
-				Type:     s.getTargetType(info.Name()),
-				Selected: false,
+			
+			if s.isCleanupTarget(info.Name()) {
+				return filepath.SkipDir
 			}
-
-			s.targets = append(s.targets, target)
-
-			return filepath.SkipDir
 		}
-
+		
 		return nil
 	})
 }
+
+func (s *Scanner) workerNoProgress(workQueue <-chan workItem, resultQueue chan<- scanResult, wg *sync.WaitGroup) {
+	defer wg.Done()
+	
+	for item := range workQueue {
+		if s.isCleanupTarget(item.info.Name()) {
+			size := s.calculateDirSizeConcurrent(item.path)
+			
+			target := CleanupTarget{
+				Path:     item.path,
+				Name:     item.info.Name(),
+				Size:     size,
+				Type:     s.getTargetType(item.info.Name()),
+				Selected: false,
+			}
+			
+			resultQueue <- scanResult{target: target, err: nil}
+		} else {
+			resultQueue <- scanResult{target: CleanupTarget{}, err: nil}
+		}
+	}
+}
+
 
 func (s *Scanner) isCleanupTarget(name string) bool {
 	_, exists := CommonCleanupDirs[name]
@@ -182,26 +332,14 @@ func (s *Scanner) getTargetType(name string) string {
 	return "Unknown"
 }
 
-func (s *Scanner) calculateDirSize(dirPath string) (int64, error) {
-	var size int64
-
-	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return nil
-		}
-
-		if !info.IsDir() {
-			size += info.Size()
-		}
-
-		return nil
-	})
-
-	return size, err
-}
 
 func (s *Scanner) GetTargets() []CleanupTarget {
-	return s.targets
+	s.targetsMutex.RLock()
+	defer s.targetsMutex.RUnlock()
+	
+	targets := make([]CleanupTarget, len(s.targets))
+	copy(targets, s.targets)
+	return targets
 }
 
 func (s *Scanner) GetWorkingDir() string {
